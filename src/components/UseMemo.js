a9dd9daa3c24ecb1d11bab20ce04428cbe04b284
Sakshi <sakshichoudhary000@gmail.com>
2024-07-27 02:12:54 +0530

    hadd ho gayi

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,12 +1,19 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 function UseMemo({ todos }) {
-  const count = useMemo(() => todos.length, [todos]);
-
+      const [count, setCount] = useState(0);
+  const NoOfTask = useMemo(() => todos.length, [todos]);
+  const handleIncrement = () => {
+    setCount(count + 1);
+    console.log(`count after increment ${count}`);
+  };
   return (
     <div>
       <h2>UseMemo Component</h2>
-      <p id="incr-cnt">{count}</p>
+      <p>No of Tasks: {NoOfTask}</p>
+      <button id="incr-cnt" onClick={handleIncrement}>
+        {count}
+      </button>
       <ul>
         {todos.map((todo, index) => (
           <li id={`todo-${index}`} key={index}>{todo}
